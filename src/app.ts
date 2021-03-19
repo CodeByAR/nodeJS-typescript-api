98cf42e0716fc7a1b1be8bb3fc529118f83eb6c5
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-19 18:24:04 +0530

    added swagger and method description for health-check

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import routes from "./routes/routes";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,14 +9,15 @@ const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,POST');
-  res.append('Content-Type', 'application/json');
+  //res.append('Content-Type', 'application/json');
   res.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
   next();
 });
 
 app.use(express.json());
 app.use("/api", routes);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(port, () => {
+app.listen(port, () => {  
   console.log(`App is running on port: ${port}`);
 });
